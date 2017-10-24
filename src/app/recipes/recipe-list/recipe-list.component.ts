d22bf0dfd87c5a0d20eaf0dc5fe7011025dfa12d
Mackander Singh <m.singh@iaea.org>
2017-10-24 17:57:08 +0200

    Added components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','This is simply a test','https://get.pxhere.com/photo/dish-meal-food-produce-vegetable-recipe-eat-lunch-cuisine-pizza-cheese-cook-tomatoes-quiche-olives-kohlrabi-italian-food-au-gratin-gratin-1292471.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
