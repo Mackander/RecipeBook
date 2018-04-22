3f76dacc05413cc4221a5490cb1368da861a42e7
mackander <mackander@gmail.com>
2018-04-22 11:46:00 +0200

    replacing Http with HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
@@ -18,7 +18,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     ReactiveFormsModule,
     SharedModule,
     ShoppingListModule,
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,31 +1,33 @@
+import { Recipe } from './../recipes/recipe.model';
 import { AuthService } from './../auth/auth.service';
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
-import { Recipe } from '../recipes/recipe.model';
+//import { Recipe } from '../recipes/recipe.model';
 import * as firebase from 'firebase';
 
 @Injectable()
 
 export class DataStorageService {
-    constructor(private http: Http,
+    constructor(private httpClient: HttpClient,
         private recipeService: RecipeService,
         private authService: AuthService) { }
 
 
     storeRecipies() {
         const token = this.authService.getToken();
-        return this.http.put('https://ng-recipe-book-cee0c.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+        return this.httpClient.put('https://ng-recipe-book-cee0c.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
     }
 
 
     getRecipiesData() {
         const token = this.authService.getToken();
 
-        return this.http.get('https://ng-recipe-book-cee0c.firebaseio.com/recipes.json?auth=' + token)
+        //we can tell httpClient what type of data are we getting back
+        //So "get" can be generic type and we can specify what type of data we are expecting
+        return this.httpClient.get<Recipe[]>('https://ng-recipe-book-cee0c.firebaseio.com/recipes.json?auth=' + token)
             .map(
-            (response: Response) => {
-                const recipes: Recipe[] = response.json();
+            (recipes) => {
                 recipes.forEach((recipe: Recipe) => {
                     if (recipe['ingredients'] == null) {
                         recipe['ingredients'] = [];
