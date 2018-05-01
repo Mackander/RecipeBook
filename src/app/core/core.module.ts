be4dcaa7188f61bdd64d6175103b402d6710b4c7
mackander <mackander@gmail.com>
2018-05-01 22:43:52 +0200

    intercepting response - lesson 298
    Added logging.interceptor.ts file to logging data from the response

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -9,6 +9,7 @@ import { SharedModule } from './../shared/shared.module';
 import { HeaderComponent } from './../core/header/header.component';
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoggingInterceptor } from '../shared/logging,interceptor';
 
 
 @NgModule({
@@ -16,7 +17,10 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
     imports: [SharedModule, AppRoutingModule],
     exports: [HeaderComponent, AppRoutingModule],
     providers: [ShoppingListSerivice, RecipeService, DataStorageService, AuthService,
+        //Note the execution order of Interceptors depending on order of defining it 
+        // So AuthInterceptor execute first and then LoggingInterceptor
         { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true } // lecture 296
+        ,{ provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true } // lecture 298
     ],
 })
 export class CoreModule { }
---
+++ src/app/shared/logging,interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpEvent, HttpHandler, HttpRequest, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do'
+
+export class LoggingInterceptor implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        //To intercept response we use 'do' method instead of subscribe method as it consumes the response
+        //and do allow us to execute code without comsuming it.
+        return next.handle(req).do(
+            event => {
+                console.log('Logging Interceptor', event);
+            }
+        );
+    }
+
+}
